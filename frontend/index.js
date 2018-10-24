0c53c25881e6afb0bb0d8fdbdca6a27c9c756142
fraser <mrfrase3@gmail.com>
2018-10-24 19:31:03 +0800

    implement group routing

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,3 @@
-import router from '@/router';
 import plugin from '../plugin.json';
-import placehold from './placehold.vue';
-
-router.addRoutes([
-  {
-    path: '/:groupId/content',
-    component: placehold,
-  },
-]);
 
 export default plugin;
--- frontend/placehold.vue
+++ frontend/placehold.vue
@@ -1,3 +1,23 @@
 <template>
-  <span>Content Placeholder</span>
+  <span>Content Placeholder for {{current.name}}</span>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  data() {
+    return {
+      current: this.currentGroup || {},
+    };
+  },
+  computed: {
+    ...mapGetters('groups', { currentGroup: 'current' }),
+  },
+  watch: {
+    currentGroup() {
+      this.current = this.currentGroup || {};
+    },
+  },
+};
+</script>
