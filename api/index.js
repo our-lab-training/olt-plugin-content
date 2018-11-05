91bebb7333ef26538615f9529b0aacc4cb6b9e2e
fraser <mrfrase3@gmail.com>
2018-11-05 11:47:51 +0800

    remove config schema, no longer using that system

--- api/index.js
+++ api/index.js
@@ -1,7 +1,5 @@
 const services = require('./services');
-const configSchema = require('./config-schema');
 
 module.exports = (app) => {
   app.configure(services);
-  app.configure(configSchema);
 };
