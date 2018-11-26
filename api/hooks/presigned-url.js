98d5d2e370bb2ae593961ff246db2d8b8470fae7
mrfrase3 <mrfrase3@gmail.com>
2018-11-26 00:58:19 +0000

    return expiry with presigned url

--- api/hooks/presigned-url.js
+++ api/hooks/presigned-url.js
@@ -2,24 +2,32 @@
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
 const errors = require('@feathersjs/errors');
+const expiry = 900;
 
 // eslint-disable-next-line no-unused-vars
 module.exports = function (options = {}) {
   return context => {
     return new Promise( (resolve, reject) => {
-      const { method, app, result } = context;
+      const { method, app, result, data } = context;
       const {key} = result;
 
-      if (method === 'get') {
-        app.buckets.private.getSignedUrl('getObject', {Key: key}, (err, url) => {
+      // depending on the method, get a diff type of presigned
+      if (method === 'get') { // read
+        app.buckets.private.getSignedUrl('getObject', { Key: key, Expires: expiry }, (err, url) => {
           if(err){
             console.error(err); // eslint-disable-line no-console
             return reject(new errors.GeneralError('Could not initiate download link, please contact an administrator.'));
           }
-          context.result.url = url;
+          context.result.presign = {
+            url,
+            expiry: Date.now() + (expiry * 1000),
+          };
           resolve(context);
         });
-      } else {
+      } else { // write
+        // we know this is only sent at the end of an exchange, where a presign is not needed
+        if (data.md5) return resolve(context);
+        // this allows us to add restrictions to a write, like the file size
         app.buckets.private.createPresignedPost({
           Bucket: result.bucket,
           Fields: {
@@ -30,11 +38,15 @@ module.exports = function (options = {}) {
             ['content-length-range', 0, 1048576],
             { 'success_action_status': '200' },
           ],
-          Expires: 900,
+          Expires: expiry,
         }, (err, data) => {
           data.fields.success_action_status = '200';
-          context.result.fields = data.fields;
-          context.result.url = data.url;
+          // fields are a set of fields that must be included with the file upload.
+          context.result.presign = {
+            fields: data.fields,
+            url: data.url,
+            expiry: Date.now() + (expiry * 1000),
+          };
           resolve(context);
         });
       }
