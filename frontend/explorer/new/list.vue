392deae7d9dbf60c2162681f38fb98046e25c4a0
Fraser Bullock <mrfrase3@gmail.com>
2019-03-07 17:37:14 +0800

    fix new doc not opening

--- frontend/explorer/new/list.vue
+++ frontend/explorer/new/list.vue
@@ -1,20 +1,20 @@
 <template>
   <v-list>
-    <v-list-tile to="#" @click.prevent="openDialog('markdown')">
+    <v-list-tile @click.stop="openDialog('markdown')">
       <v-list-tile-action><v-icon>fal fa-file-invoice</v-icon></v-list-tile-action>
       <v-list-tile-title>New Document</v-list-tile-title>
     </v-list-tile>
-    <v-list-tile to="#" @click.prevent="openDialog('text')">
+    <v-list-tile @click.stop="openDialog('text')">
       <v-list-tile-action><v-icon>fal fa-file-alt</v-icon></v-list-tile-action>
       <v-list-tile-title>New Text File</v-list-tile-title>
     </v-list-tile>
     <v-divider></v-divider>
-    <v-list-tile to="#" @click.prevent="openDialog('directory')">
+    <v-list-tile @click.stop="openDialog('directory')">
       <v-list-tile-action><v-icon>fal fa-folder-plus</v-icon></v-list-tile-action>
       <v-list-tile-title>New Folder</v-list-tile-title>
     </v-list-tile>
     <v-divider></v-divider>
-    <v-list-tile to="#" @click.prevent="openDialog('upload')">
+    <v-list-tile @click.stop="openDialog('upload')">
       <v-list-tile-action><v-icon>fal fa-cloud-upload</v-icon></v-list-tile-action>
       <v-list-tile-title>Upload File</v-list-tile-title>
     </v-list-tile>
