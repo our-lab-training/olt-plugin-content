02d2ab203369b5ae4fc85abfd1a9a78cf2095916
mrfrase3 <mrfrase3@gmail.com>
2019-06-29 23:40:00 +0800

    make compatible with being used outside of the content explorer

--- frontend/explorer/view/msdoc.vue
+++ frontend/explorer/view/msdoc.vue
@@ -1,8 +1,9 @@
 <template>
   <v-card>
     <v-toolbar dense>
-      <back-btn />
-      <v-toolbar-title>
+      <back-btn v-if="back"/>
+      <v-toolbar-title v-if="title">{{title}}</v-toolbar-title>
+      <v-toolbar-title v-else>
         <span
           class="text-capitalize"
           ref="filename"
@@ -17,9 +18,13 @@
           label="Renderer"
         />
       </v-flex>
+      <v-btn flat icon v-if="toggleHidden" @click="$emit('update:hidden', !hidden)">
+        <v-icon>far fa-{{hidden ? 'chevron-up' : 'chevron-down'}}</v-icon>
+      </v-btn>
     </v-toolbar>
     <iframe
       v-if="url"
+      :style="!toggleHidden || !hidden ? {} : { height: '0' }"
       :src="url"
       frameborder='0'
     >
@@ -39,6 +44,28 @@ export default {
   components: {
     backBtn,
   },
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    fileId: {
+      type: String,
+      default: '',
+    },
+    back: {
+      type: Boolean,
+      default: true,
+    },
+    hidden: {
+      type: Boolean,
+      default: false,
+    },
+    toggleHidden: {
+      type: Boolean,
+      default: false,
+    },
+  },
   data() {
     return {
       presign: null,
@@ -46,7 +73,7 @@ export default {
     };
   },
   computed: {
-    ...mapGetters('content', ['current']),
+    ...mapGetters('content', { currentContent: 'current', getContent: 'get' }),
     ...mapState('content', ['isOperationPending']),
     ext() { return this.current.filename.split('.').pop(); },
     path() { return this.$route.params.path; },
@@ -57,20 +84,31 @@ export default {
         ? `https://docs.google.com/gview?url=${this.presign.url.replace(/[&]/g, '%26')}&embedded=true`
         : `https://view.officeapps.live.com/op/embed.aspx?src=${this.presign.url.replace(/[&]/g, '%26')}`;
     },
+    current() { return this.fileId ? this.getContent(this.fileId) : this.currentContent; },
   },
-  mounted() {
-    setTimeout(async () => {
+  methods: {
+    async getPresign() {
       if (!this.current) return console.log('nope');
       const content = await this.$content._getPresign(this.current, 'read');
       this.presign = content.presign;
       return content;
-    }, 200);
+    },
+    getPresignStandoff() {
+      setTimeout(async () => {
+        if (!(await this.getPresign())) {
+          setTimeout(async () => {
+            if (!(await this.getPresign())) setTimeout(async () => this.getPresign(), 300);
+          }, 200);
+        }
+      }, 100);
+    },
+  },
+  mounted() {
+    this.getPresignStandoff();
   },
   watch: {
-    async path() {
-      if (!this.current) return;
-      const content = await this.$content._getPresign(this.current, 'read');
-      this.presign = content.presign;
+    path() {
+      this.getPresignStandoff();
     },
   },
 };
